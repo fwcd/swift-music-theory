67117c9ea49ff60ba898baba86f2f1ba4eaa0cae
fwcd <fwcdmail@gmail.com>
2020-09-19 18:04:52 +0200

    Add PentatonicScale

---
+++ Sources/MusicTheory/scale/PentatonicScale.swift
@@ -0,0 +1,15 @@
+struct PentatonicScale: Scale {
+    public let key: Note
+    
+    public var notes: [Note] { [
+        key,
+        key + .majorSecond,
+        key + .majorThird,
+        key + .perfectFifth,
+        key + .majorSixth
+    ] }
+
+    public init(key: Note) {
+        self.key = key
+    }
+}
