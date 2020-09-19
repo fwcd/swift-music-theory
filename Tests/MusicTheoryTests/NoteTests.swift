35f499af302777d8d038b658137d7681986e2fc7
fwcd <fwcdmail@gmail.com>
2020-09-19 17:53:54 +0200

    Test notes and intervals

--- Tests/MusicTheoryTests/NoteTests.swift
+++ Tests/MusicTheoryTests/NoteTests.swift
@@ -3,14 +3,21 @@ import XCTest
 
 final class NoteTests: XCTestCase {
     static var allTests = [
-        ("testNote", testNote)
+        ("testNote", testNote),
+        ("testNoteIntervals", testNoteIntervals)
     ]
     
-    func testNote() throws {
+    func testNote() {
         XCTAssertEqual(Note(.c, 0).numValue, 0)
         XCTAssertEqual(Note(.c, 1).numValue, 12)
         XCTAssertEqual(Note(.d, 0).numValue, 2)
         XCTAssertEqual(Note(.d, .sharp, 0).numValue, 3)
         XCTAssertEqual(Note(.e, .sharp, 4).numValue, Note(.f, 4).numValue)
+    }
+
+    func testNoteIntervals() {
+        XCTAssertEqual(Note(.c, 0) + .octave, Note(.c, 1))
+        XCTAssertEqual(Note(.c, 1) + .minorSecond, Note(.d, .flat, 1))
+        XCTAssertEqual(Note(.c, 1) + .majorSecond, Note(.d, 1))
     }
 }
