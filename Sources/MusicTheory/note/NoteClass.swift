f955f58b24dfc8f4a0ea4a31b76e5c8920f58f12
fwcd <fwcdmail@gmail.com>
2020-09-19 16:14:58 +0200

    Add twelve-tone octave to NoteClass

--- Sources/MusicTheory/note/NoteClass.swift
+++ Sources/MusicTheory/note/NoteClass.swift
@@ -2,6 +2,23 @@
 /// (theoretical) pitch class, it does, however,
 /// specify an enharmonic spelling.
 public struct NoteClass: Codable, Hashable {
+    /// The twelve note classes with their (most common)
+    /// enharmonic spellings.
+    public static let twelveToneOctave: [[NoteClass]] = [
+        [Self(.c)],
+        [Self(.c, .sharp), Self(.d, .flat)],
+        [Self(.d)],
+        [Self(.d, .sharp), Self(.e, .flat)],
+        [Self(.e)],
+        [Self(.f)],
+        [Self(.f, .sharp), Self(.g, .flat)],
+        [Self(.g)],
+        [Self(.g, .sharp), Self(.a, .flat)],
+        [Self(.a)],
+        [Self(.a, .sharp), Self(.b, .flat)],
+        [Self(.b)]
+    ]
+
     public var letter: NoteLetter
     public var accidental: NoteAccidental?
 
