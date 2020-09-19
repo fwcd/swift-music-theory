88f1ccad994e5f4e11badaaa0836408cd3abbcfa
fwcd <fwcdmail@gmail.com>
2020-09-19 18:16:06 +0200

    Fix octaves in DiatonicMajorScaleTests

--- Tests/MusicTheoryTests/scale/DiatonicMajorScaleTests.swift
+++ Tests/MusicTheoryTests/scale/DiatonicMajorScaleTests.swift
@@ -20,21 +20,19 @@ final class DiatonicMajorScaleTests: XCTestCase {
             Note(.g, 0),
             Note(.a, 0),
             Note(.b, 0),
-            Note(.c, 0),
-            Note(.d, 0),
-            Note(.e, 0),
-            Note(.f, .sharp, 0),
-            Note(.g, 0)
+            Note(.c, 1),
+            Note(.d, 1),
+            Note(.e, 1),
+            Note(.f, .sharp, 1)
         ])
         XCTAssertEqual(DiatonicMajorScale(key: Note(.g, .flat, 0)).notes, [
             Note(.g, .flat, 0),
             Note(.a, .flat, 0),
             Note(.b, .flat, 0),
-            Note(.c, .flat, 0),
-            Note(.d, .flat, 0),
-            Note(.e, .flat, 0),
-            Note(.f, .flat, 0),
-            Note(.g, .flat, 0)
+            Note(.c, .flat, 0), // TODO: Should this be denoted using octave 1?
+            Note(.d, .flat, 1),
+            Note(.e, .flat, 1),
+            Note(.f, .flat, 1)
         ])
     }
 }
