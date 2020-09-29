50160342c729c90a1b43d23e96f27b258bce9ccc
fwcd <fwcdmail@gmail.com>
2020-09-29 22:01:36 +0200

    Update package name to match repo name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "MusicTheory",
+    name: "swift-music-theory",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
