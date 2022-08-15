d8835b443cff76ba2366759a8cd1f5d353a8f451
fwcd <fwcdmail@gmail.com>
2022-08-15 18:16:42 +0200

    Test note letter arithmetic

--- Tests/MusicTheoryTests/note/NoteLetterTests.swift
+++ Tests/MusicTheoryTests/note/NoteLetterTests.swift
@@ -2,10 +2,12 @@ import XCTest
 @testable import MusicTheory
 
 final class NoteLetterTests: XCTestCase {
-    func testNoteLetter() throws {
+    func testArithmetic() throws {
         XCTAssertEqual(NoteLetter.c.advanced(by: 0), .c)
         XCTAssertEqual(NoteLetter.d.advanced(by: 2), .f)
         XCTAssertEqual(NoteLetter.e.advanced(by: 7), .e)
         XCTAssertEqual(NoteLetter.c.advanced(by: -1), .b)
+
+        XCTAssertEqual(NoteLetter.f.advanced(by: 15), NoteLetter.f + 15)
     }
 }
