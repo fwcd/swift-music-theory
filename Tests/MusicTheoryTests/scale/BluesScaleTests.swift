77f6d5c891e01d300734a9678a4a10f3f9c8e4e8
fwcd <fwcdmail@gmail.com>
2022-08-15 18:39:40 +0200

    Test major blues scale

--- Tests/MusicTheoryTests/scale/BluesScaleTests.swift
+++ Tests/MusicTheoryTests/scale/BluesScaleTests.swift
@@ -2,7 +2,18 @@ import XCTest
 @testable import MusicTheory
 
 final class BluesScaleTests: XCTestCase {
-    func testMinorScale() throws {
+    func testMajorScale() {
+        XCTAssertEqual(MajorBluesScale(key: Note(.g, 1)).notes, [
+            Note(.g, 1),
+            Note(.a, 1),
+            Note(.b.flat, 1),
+            Note(.b, 1),
+            Note(.d, 2),
+            Note(.e, 2),
+        ])
+    }
+
+    func testMinorScale() {
         XCTAssertEqual(MinorBluesScale(key: Note(.c, 0)).notes, [
             Note(.c, 0),
             Note(.e.flat, 0),
