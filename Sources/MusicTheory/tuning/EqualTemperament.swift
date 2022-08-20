b7ad3c4f191050a8d6fe082b02aeb3618c86dbf5
fwcd <fwcdmail@gmail.com>
2022-08-20 16:23:21 +0200

    Make EqualTemperament initializer more general

--- Sources/MusicTheory/tuning/EqualTemperament.swift
+++ Sources/MusicTheory/tuning/EqualTemperament.swift
@@ -2,9 +2,11 @@ import Foundation
 
 /// The standard, twelve-tone equal temperament tuning.
 public struct EqualTemperament: Tuning {
-    public let a4PitchHz: Double
+    private let referencePitchHz: Double
+    private let referencePitchSemitone: Int
 
-    // The conversion function can be derived as a solution of the following recursive system of equations:
+    // The conversion function can be derived as a solution of the following recursive system of equations
+    // (for simplicity we'll assume referencePitchHz = 440, referencePitchSemitone = 57):
     //
     //   2 * f(n) = f(n + 12)    (moving up 12 semitones should double the pitch/frequency)
     //      f(57) = 440          (57 = 12 * 4 + 9 is the semitone of A4)
@@ -12,25 +14,29 @@ public struct EqualTemperament: Tuning {
     // Substituting in a generic exponential function c * b^n and solving yields:
     //
     //   b = 12th root of 2
-    //   c = a4PitchHz / (12th root of 2)^57
+    //   c = 440 / (12th root of 2)^57
     //
     // Simplifying gets us
     // 
     //   f(n) = c * b^n
-    //        = a4PitchHz * (1 / (12th root of 2)^57) * 2^(n / 12)
-    //        = a4PitchHz * 2^(-57 / 12) * 2^(n / 12)
-    //        = a4PitchHz * 2^((n - 57) / 12)
+    //        = 440 * (1 / (12th root of 2)^57) * 2^(n / 12)
+    //        = 440 * 2^(-57 / 12) * 2^(n / 12)
+    //        = 440 * 2^((n - 57) / 12)
 
-    public init(a4PitchHz: Double = 440) {
-        self.a4PitchHz = a4PitchHz
+    public init(
+        referencePitchHz: Double = 440,
+        referencePitchSemitone: Int = Note(.a, 4).semitone
+    ) {
+        self.referencePitchHz = referencePitchHz
+        self.referencePitchSemitone = referencePitchSemitone
     }
 
     public func pitchHz(for note: Note) -> Double {
-        a4PitchHz * pow(2, Double(note.semitone - 57) / 12.0)
+        referencePitchHz * pow(2, Double(note.semitone - referencePitchSemitone) / 12.0)
     }
 
     public func semitone(for pitchHz: Double) -> Double {
         assert(pitchHz > 0)
-        return 12.0 * log2(pitchHz / a4PitchHz) + 57
+        return 12.0 * log2(pitchHz / referencePitchHz) + Double(referencePitchSemitone)
     }
 }
